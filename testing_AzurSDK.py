25e871af756b0498a5ab517d52f51eb6b4b9f2b0
Abdielfer <76761139+Abdielfer@users.noreply.github.com>
2025-12-29 23:46:45 -0500

    Agregar testing_AzurSDK.py y configurar entorno virtual con Python 3.14

---
+++ testing_AzurSDK.py
@@ -0,0 +1,22 @@
+from azure.identity import DefaultAzureCredential
+from azure.ai.projects import AIProjectClient
+...
+
+
+try:
+
+# Get project client
+project_endpoint = "https://aiagentictraining-resource.services.ai.azure.com/api/projects/abdielfer-aiagentictraining"
+project_client = AIProjectClient(            
+        credential=DefaultAzureCredential(),
+        endpoint=project_endpoint,
+    )
+
+## List all connections in the project
+connections = project_client.connections
+print("List all connections:")
+for connection in connections.list():
+    print(f"{connection.name} ({connection.type})")
+
+except Exception as ex:
+    print(ex)
